f517adef9ac6d6b5fa4d73f78502aaa9262c696a
BillionsRichard <295292802@qq.com>
2017-07-08 22:25:01 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ print 'hello'
 edited on github again
 #this is modified in working tree.
 ＃测试远端仓库有更新，本地也更新了同一个文件时，如果本地的更新添加到缓存区，ｇit pull的行为如何？
-#测试本地修改如果缓存了，git pull会获取远端最新代码到本地工作区
+#测试本地修改如果提交或者stash了，git pull才会获取远端最新代码到本地工作区，否则会提示冲突。（modified on github）
